f8a77ee84f74fff4f8ca80aed0626a233336ee7b
peanuts <849421294godw@gmail.com>
2021-04-28 02:52:58 +0800

    change fielf for model user

--- src/api/model/user.go
+++ src/api/model/user.go
@@ -4,17 +4,19 @@ import "github.com/bwmarrin/snowflake"
 
 //User means a people who use the system.
 type User struct {
-	UID     int64
-	Account string
-	Passwd  string
+	UID      int64
+	Account  string
+	Password string
 }
 
 //NewUser returns a User who UID generate by snowlake Algorithm
-func NewUser(account string, passwd string) *User {
+func NewUser(account string, password string) *User {
 	node, _ := snowflake.NewNode(1)
 	return &User{
-		UID:     node.Generate().Int64(),
-		Account: account,
-		Passwd:  passwd,
+		UID:      node.Generate().Int64(),
+		Account:  account,
+		Password: password,
 	}
 }
+
+
