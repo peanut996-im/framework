cfdb1b67603cb2418c776ab578b173cde8c27efe
peanuts <849421294godw@gmail.com>
2021-04-28 00:03:48 +0800

    add some constants

--- src/net/net.go
+++ src/net/net.go
@@ -1,9 +1,16 @@
 package net
 
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
 const (
 	// response code
-	ERROR_CODE_OK       = 0
-	ERROR_TOKEN_INVALID = 1000 + iota
+	ERROR_CODE_OK           = 0
+	ERROR_HTTP_SIGN_INVAILD = 1000 + iota
+	ERROR_TOKEN_INVALID
 	ERROR_HTTP_INNER_ERROR
 	ERROR_HTTP_PARAM_INVALID
 
@@ -28,13 +35,26 @@ const (
 var (
 	respCodeInfo = map[int]string{
 		ERROR_CODE_OK:            "Success",
-		ERROR_TOKEN_INVALID:      "Token invalid err: %v",
+		ERROR_TOKEN_INVALID:      "Token invalid.",
 		ERROR_HTTP_INNER_ERROR:   "Http inner error err: %v",
 		ERROR_HTTP_PARAM_INVALID: "Http param invalid err: %v",
+		ERROR_HTTP_SIGN_INVAILD:  "Sign invaild.",
 		// ERROR_REDIS:              "Redis error err: %v",
 		// ERROR_MONGO:              "Mongo error err: %v",
 		// ERROR_UNMARSHAL_JSON:     "Unmarshal json  err: %v",
 		// ERROR_MARSHAL_JSON:       "Marshal json  err: %v",
+	}
+
+	InnerErrorResp = &BaseRepsonse{
+		Code:    ERROR_HTTP_INNER_ERROR,
+		Message: fmt.Sprintf(ErrorCodeToString(ERROR_HTTP_INNER_ERROR)),
+		Data:    struct{}{},
+	}
+
+	SignInvaildResp = &BaseRepsonse{
+		Code:    ERROR_HTTP_SIGN_INVAILD,
+		Message: fmt.Sprintf(ErrorCodeToString(ERROR_HTTP_SIGN_INVAILD)),
+		Data:    struct{}{},
 	}
 )
 
@@ -46,4 +66,13 @@ type BaseRepsonse struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+}
+
+func ReadFromBody(rc io.Reader) (string, error) {
+	sb := new(strings.Builder)
+	_, err := io.Copy(sb, rc)
+	if err != nil {
+		return "", err
+	}
+	return sb.String(), nil
 }
