3af0fd999057c4d3db5eafbac59430e6a4fe14a8
peanuts <849421294godw@gmail.com>
2021-04-28 01:11:01 +0800

    fix body eof

--- src/net/http/middleware.go
+++ src/net/http/middleware.go
@@ -1,10 +1,12 @@
 package http
 
 import (
+	"bytes"
 	"framework/api"
 	"framework/cfgargs"
 	"framework/logger"
 	"framework/net"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,16 +14,27 @@ import (
 
 func CheckSign(cfg *cfgargs.SrvConfig) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		j, err := net.ReadFromBody(c.Request.Body)
+		// j, err := net.ReadStringFromBody(c.Request.Body)
+		b, err := c.GetRawData()
+		if err != nil {
+			logger.Error("get raw data err: %v", err)
+			c.AbortWithStatusJSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
+		}
+		// write data to body
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(b))
 
-		if err != nil || len(j) == 0 {
+		body := string(b)
+
+		logger.Debug("get body : %v", body)
+
+		if err != nil || len(body) == 0 {
 			checkResult, err := api.CheckSignFromQueryParams(c.Request.URL.Query(), cfg.AppKey)
 			if !checkResult || err != nil {
 				logger.Debug("check sign with query string failed")
 				c.AbortWithStatusJSON(http.StatusOK, net.SignInvaildResp)
 			}
 		} else {
-			checkResult, err := api.CheckSignFromJsonString(j, cfg.AppKey)
+			checkResult, err := api.CheckSignFromJsonString(body, cfg.AppKey)
 			if !checkResult || err != nil {
 				logger.Debug("check sign with json failed")
 				c.AbortWithStatusJSON(http.StatusOK, net.SignInvaildResp)
