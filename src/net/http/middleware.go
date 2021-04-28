926fad796397c247b3f3d1cab558f54e34e064d8
peanuts <849421294godw@gmail.com>
2021-04-29 02:39:11 +0800

    checksign add debug option

--- src/net/http/middleware.go
+++ src/net/http/middleware.go
@@ -14,27 +14,40 @@ import (
 
 func CheckSign(cfg *cfgargs.SrvConfig) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// j, err := net.ReadStringFromBody(c.Request.Body)
 		b, err := c.GetRawData()
 		if err != nil {
 			logger.Error("get raw data err: %v", err)
 			c.AbortWithStatusJSON(http.StatusOK, net.NewHttpInnerErrorResp(err))
 		}
-		// write data to body
 		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(b))
 
 		body := string(b)
 
 		if err != nil || len(body) == 0 {
+			// query string
 			checkResult, err := api.CheckSignFromQueryParams(c.Request.URL.Query(), cfg.AppKey)
 			if !checkResult || err != nil {
 				logger.Debug("check sign with query string failed")
+				if !cfg.HTTP.Release {
+					sign, err := api.MakeSignWithQueryParams(c.Request.URL.Query(), cfg.AppKey)
+					if err == nil {
+						c.AbortWithStatusJSON(http.StatusOK, net.NewBaseResponse(net.ERROR_SIGN_INVAILD, gin.H{"sign": sign}))
+						return
+					}
+				}
 				c.AbortWithStatusJSON(http.StatusOK, net.SignInvaildResp)
 			}
 		} else {
+			// json
 			checkResult, err := api.CheckSignFromJsonString(body, cfg.AppKey)
 			if !checkResult || err != nil {
 				logger.Debug("check sign with json failed")
+				if !cfg.HTTP.Release {
+					sign, err := api.MakeSignWithJsonString(body, cfg.AppKey)
+					if err == nil {
+						c.AbortWithStatusJSON(http.StatusOK, net.NewBaseResponse(net.ERROR_SIGN_INVAILD, gin.H{"sign": sign}))
+					}
+				}
 				c.AbortWithStatusJSON(http.StatusOK, net.SignInvaildResp)
 			}
 		}
