150bf6667a118cfd6819a1f7cb224f78df10bbe7
peanuts <849421294godw@gmail.com>
2021-05-22 16:27:33 +0800

    refactor: remove CRUD to model package.

--- src/api/model/user.go
+++ src/api/model/user.go
@@ -1,30 +1,59 @@
 package model
 
 import (
-	"github.com/bwmarrin/snowflake"
+	"framework/db"
+	"framework/encoding"
+	"framework/logger"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 //User means a people who use the system.
 type User struct {
-	UID      string
-	Account  string
-	Password string
+	UID      string `json:"uid,omitempty"`
+	Account  string `json:"account"`
+	Password string `json:"-"`
 }
 
 //NewUser returns a User who UID generate by snowlake Algorithm
 func NewUser(account string, password string) *User {
-	node, _ := snowflake.NewNode(1)
 	return &User{
-		UID:      node.Generate().String(),
+		UID:      encoding.NewSnowFlakeID(),
 		Account:  account,
 		Password: password,
 	}
 }
 
-func GetUserByAccount(account string) *User {
-	return nil
+func GetUserByAccount(account string) (*User, error) {
+	mongo := db.GetLastMongoClient()
+	filter := bson.M{"account": account}
+	user := &User{}
+	err := mongo.FindOne("User", user, filter)
+	if err != nil {
+		logger.Info("mongo get user from account err: %v, uid: %v", err, account)
+		return nil, err
+	}
+	return user, nil
 }
 
-func GetUserByUID(uid string) *User {
+func GetUserByUID(uid string) (*User, error) {
+	mongo := db.GetLastMongoClient()
+	filter := bson.M{"uid": uid}
+	user := &User{}
+	err := mongo.FindOne("User", user, filter)
+	if err != nil {
+		logger.Info("mongo get user from uid err: %v, uid: %v", err, uid)
+		return nil, err
+	}
+	return user, nil
+}
+
+func InsertUser(user *User) error {
+	mongo := db.GetLastMongoClient()
+	res, err := mongo.InsertOne("User", user)
+	if err != nil {
+		logger.Error("mongo insert user err: %v", err)
+		return err
+	}
+	logger.Info("Mongo insert user success, id: %v", res.InsertedID)
 	return nil
 }
